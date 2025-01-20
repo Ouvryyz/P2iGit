a469fc3628e4e27c0b69633f02e605944732867f
Ouvryyz <clementouvry1@gmail.com>
2025-01-20 15:07:51 +0100

    MoveAndVelocity

--- Fighter/Scenes/Player1/Scripts/player_1.gd
+++ Fighter/Scenes/Player1/Scripts/player_1.gd
@@ -1,31 +1,35 @@
-extends Node2D
-class_name Player1
+extends CharacterBody2D
+class_name Player
 
 @export_range(0.0,1.0) var accel_factor : float = 01
 @export_range(0.0,1.0) var rotation_accel_factor : float = 01
+
 
 @export var max_speed : float = 200.0
 var speed : float = 0.0
 
 var direction := Vector2.ZERO
 var Last_direction := Vector2.ZERO
-var velocity: Vector2 = Vector2.ZERO
-
-
 
 
 func _ready() -> void:
 	pass
 
+
 func _physics_process(delta: float) -> void:
 	move()
 	rotate_toward_mouse()
+
 
 func _input(event: InputEvent) -> void:
 	direction=Input.get_vector("move_left","move_right","move_up","move_down")
 	print(direction)
 	if direction != Vector2.ZERO:
 		Last_direction = direction
+	
+
+
+
 
 func move()-> void:
 	
@@ -34,8 +38,10 @@ func move()-> void:
 	else:	
 		speed=lerp(speed,max_speed,accel_factor)
 		
-	velocity = Last_direction * speed
 	
+	velocity = Last_direction * speed
+	move_and_slide()
+
 
 func rotate_toward_mouse()-> void:
 	var mouse_pos = get_global_mouse_position()
