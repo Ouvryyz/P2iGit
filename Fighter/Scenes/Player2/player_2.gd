847c1adf4767f232d4d7354708112678d7b44147
Ouvryyz <clementouvry1@gmail.com>
2025-03-10 23:58:15 +0100

    VieupdatePlayer2

--- Fighter/Scenes/Player2/player_2.gd
+++ Fighter/Scenes/Player2/player_2.gd
@@ -1,14 +1,21 @@
 extends CharacterBody2D
 class_name Player2
 
-var health: int = 100  # Points de vie du Player 2
+var health: int = 100  
+
+@onready var hitbox = $HitboxArea  
+
+func _physics_process(delta: float) -> void:
+	for area in hitbox.get_overlapping_areas():
+		print("Zone détectée :", area)
+		if area.get_parent().is_in_group("player1"):
+			take_damage(20)
+			
 
 func _ready() -> void:
-<<<<<<< Updated upstream
-	hitbox.monitoring = false  
-	hitbox.monitorable = true  
-=======
 	print("Player 2 prêt avec", health, "PV")
+	hitbox.monitoring = true  
+	hitbox.connect("area_entered", Callable(self, "_on_hitbox_area_entered"))
 
 func take_damage(amount: int) -> void:
 	health -= amount
@@ -18,5 +25,12 @@ func take_damage(amount: int) -> void:
 
 func die() -> void:
 	print("Player 2 est éliminé !")
-	queue_free()  # Supprime l'objet de la scène
->>>>>>> Stashed changes
+	queue_free() 
+
+
+	 
+
+
+func _on_hitbox_area_body_entered(body: Node2D) -> void:
+	if hitbox.get_parent().is_in_group("player1"):
+		take_damage(20) 
