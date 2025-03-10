0b507b92d5df4d6a6c52839d8f254ce92143bcef
Ouvryyz <clementouvry1@gmail.com>
2025-03-10 23:47:46 +0100

    Update player_2.gd

--- Fighter/Scenes/Player2/player_2.gd
+++ Fighter/Scenes/Player2/player_2.gd
@@ -1,8 +1,22 @@
 extends CharacterBody2D
 class_name Player2
 
-@onready var hitbox = $HitboxArea
+var health: int = 100  # Points de vie du Player 2
 
 func _ready() -> void:
+<<<<<<< Updated upstream
 	hitbox.monitoring = false  
 	hitbox.monitorable = true  
+=======
+	print("Player 2 prêt avec", health, "PV")
+
+func take_damage(amount: int) -> void:
+	health -= amount
+	print("Player 2 touché ! Vie restante :", health)
+	if health <= 0:
+		die()
+
+func die() -> void:
+	print("Player 2 est éliminé !")
+	queue_free()  # Supprime l'objet de la scène
+>>>>>>> Stashed changes
